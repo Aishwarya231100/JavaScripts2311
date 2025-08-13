651d64b669cffc40f07d2c6ca7c9e5a66182073a
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-08-13 14:09:30 +0000

    Variables Done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountID = 144553  //const value does not change 
+let accountEmail = "aishwarya@gmail.com" //let mostly use and value also change 
+var accountPassword = "Aishwarya" // var not use mostly
+accountCity = "Pune" //possibility in js but not use this way 
+let accountSate; // The output is undefined 
+
+accountEmail = "ap@gmail.com"
+accountPassword = "AP"
+accountCity = "Kolhapur"
+
+console.log(accountID);
+
+//Print all in one fromat 
+console.table([accountEmail,accountPassword,accountCity]);
+
+
