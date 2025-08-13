81c58b5a423f496cd0a8ea41bf2cd0ec67d43409
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-08-13 14:50:56 +0000

    Data Type is Done

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high
+
+    let name = ("Aishwarya") //String
+    let age = 25 //Number
+    let isLoggedIn = false //Boolean
+    let state; //Undefined
+
+    /*
+    Primitive Data Types
+    Numbers = 2 to power 53
+    bignit = use in stock market big number
+    string = " " also using ' '
+    boolean = true / flase
+    null = stand alone value in js and null is special type and also empty 
+    undefine = default value
+    symbole = used in react and find the unique vale 
+
+    */ 
+
+    //check type of 
+    console.log(typeof undefined); //Output = Undefined 
+    console.log(typeof null); //Output  = Object
+    console.log(typeof name); // Output = string
+
+
+    
+    
+    
+    
