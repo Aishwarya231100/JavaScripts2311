0fc15913950563ebec3f9e958774fab9db45dcba
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-08-14 07:43:28 +0000

    Operations all done

--- 01_basics/03_ConversionOperstion.js
+++ 01_basics/03_ConversionOperstion.js
@@ -29,4 +29,56 @@ console.log(booleanIsLoggedIn);
 "Aiswarya" = true
 */
 
+let someNumber 
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber);
+
+//*************************************************Operations************************************//
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+*/
+
+let str1 = "hello"
+let str2 = " Aishwarya"
+let str3 = str1+str2
+console.log(str3);  //hello Aishwarya
+
+console.log("1" + 2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32 -------------Add value then second value print as is is
+
+console.log(3+4*5%3); // Write in this fromat console.log((3+4)*5%3);
+
+console.log(+true); //op- 1
+//console.log(true+); //op- error
+
+console.log(+" "); //op = 0
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //op = 101
+
+//or 
+
+gameCounter++
+console.log(gameCounter++); //op = 101
+
+
+
+
+
+
+
+
 
