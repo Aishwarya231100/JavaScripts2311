0cdbe6c05a27bea7d4384e67aa2a74275d504600
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-08-14 10:08:06 +0000

    Stack And Heap Done

---
+++ 01_basics/Stack and Heap.js
@@ -0,0 +1,29 @@
+/************************** Stack and Heap ************************/
+// Stack  - Use Primitive data type
+
+//let MyName = "ABC";
+//let anotherName = MyName;
+
+console.log(anotherName);  // OP = ABC 
+
+let MyName = "ABC";
+let anotherName = MyName;
+anotherName = "XYZ"      //-----------------dose not change the value
+
+console.log(anotherName);
+console.log(MyName);
+
+
+// Heap - Use non Primituve data type
+
+let UserOne{                             // You can change the origanal vaule 
+    email: "abc@gmail.com";
+    UPI: "User@ybl";
+}
+
+let UserTwo = UserOne;
+UserTwo.email = "xyz@gmail.com";
+
+console.log(UserOne);
+console.log(UserTwo);    // OP = xyz@gmail.com
+
