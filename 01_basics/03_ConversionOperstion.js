111359b61876b42db1686a456caefdb80b059f4f
Aishwarya231100 <102051465+Aishwarya231100@users.noreply.github.com>
2025-08-14 07:12:14 +0000

    Conversion Of Data Type

---
+++ 01_basics/03_ConversionOperstion.js
@@ -0,0 +1,32 @@
+let score = "33"
+
+console.log(typeof score);
+// or
+console.log(typeof (score));
+
+// Convert string to number
+let ValueInNumber = Number (score)
+console.log(typeof (ValueInNumber));
+
+/* OUTPUT
+NaN = 0
+Undefined = Nan
+True = 1
+Falsew = 0 
+"33" = 33
+"33abc" = nan
+
+*/
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+1 = true
+0 = false
+" " = false
+"Aiswarya" = true
+*/
+
+
